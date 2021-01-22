3f8fb7f7054caa30fbc5a819828b94a8e2b8f9e0
keithboice <69816300+keithboice@users.noreply.github.com>
2021-01-22 04:30:54 -0500

    updated

--- scripts/git_force_push.sh
+++ scripts/git_force_push.sh
@@ -8,24 +8,24 @@ echo "##### starting #####";
 echo ".... .github repo ....";
 cd ".conf" || exit && echo ".... in root dir ....";
 git add . || exit && echo ".... added files ....";
-git commit -m "updated" || exit && echo ".... committed files ....";
-git push -f origin main || exit && echo ".... pushed files ....";
+git commit -m "updated" || echo ".... skipping ...." && echo ".... committed files ....";
+git push -f origin main || echo ".... skipping ...." && echo ".... pushed files ....";
 
 
 echo " ";
 echo ".... core repo ....";
 cd "../" || exit && echo ".... in root dir ....";
 git add . || exit && echo ".... added files ....";
-git commit -m "updated" || exit && echo ".... committed files ....";
-git push -f origin main || exit && echo ".... pushed files ....";
+git commit -m "updated" || echo ".... skipping ...." && echo ".... committed files ....";
+git push -f origin main || echo ".... skipping ...." && echo ".... pushed files ....";
 
 
 echo " ";
 echo ".... backend repo ....";
 cd "backend" || exit && echo ".... in root dir ....";
 git add . || exit && echo ".... added files ....";
-git commit -m "updated" || exit && echo ".... committed files ....";
-git push -f origin main || exit && echo ".... pushed files ....";
+git commit -m "updated" || echo ".... skipping ...." && echo ".... committed files ....";
+git push -f origin main || echo ".... skipping ...." && echo ".... pushed files ....";
 
 
 echo "";
